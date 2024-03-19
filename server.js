caf232ef75ccae5f0928484f18caeec6001d93b3
sahil tyagi <sahiltyagi1204@outlook.com>
2024-03-19 23:59:32 +0530

    slight changes

--- server.js
+++ server.js
@@ -74,6 +74,8 @@ app.get("/", (req, res) => {
   res.sendFile("index");
 });
 
-server.listen(process.env.PORT || 3001, () => {
-  console.log("server is running");
+const PORT = process.env.PORT || 3001
+
+server.listen(PORT, () => {
+  console.log(`server is running on ${PORT} `);
 });
